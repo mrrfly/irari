156e180c7df20db15d026269427696a5d28d87b2
Muhammad Rafly Ramadhan <mrrfly.dev@gmail.com>
2020-02-06 01:29:27 +0700

    Fix Atas.php

--- application/views/template/atas.php
+++ application/views/template/atas.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 <meta name="author" content="IRARI" />
 <meta name="description" content="IRARI WEB & MOBILE DEVELOPMENT" />
 <meta name="keywords" content="IRARI, IRARI WEB & MOBILE DEVELOPMENT" />
