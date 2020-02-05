7a08e2109027e038860647b2f2bb4c7eeaa55ad2
Muhammad Rafly Ramadhan <mrrfly.dev@gmail.com>
2020-02-06 01:22:57 +0700

    Model LandingPage
    Membuat Model Landing Page

---
+++ application/models/M_landing_page.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_landing_page extends CI_Model {
+
+	
+
+}
+
+/* End of file M_landing_page.php */
+/* Location: ./application/models/M_landing_page.php */
