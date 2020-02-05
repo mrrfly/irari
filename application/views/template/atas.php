094110b01149c942a41a423c6b53ceb13e4e05f9
Muhammad Rafly Ramadhan <mrrfly.dev@gmail.com>
2020-02-06 01:04:59 +0700

    Membuat Template View
    Membuat Template Untuk Tampilan Utama Website

---
+++ application/views/template/atas.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1" />
+<meta name="author" content="IRARI" />
+<meta name="description" content="IRARI WEB & MOBILE DEVELOPMENT" />
+<meta name="keywords" content="IRARI, IRARI WEB & MOBILE DEVELOPMENT" />
+    <title>IRARI</title>
+<link rel="stylesheet" href="<?= base_url('assets/css/swiper.css') ?>">
+<link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
+</head>
+<body>
+    
---
+++ application/views/template/bawah.php
@@ -0,0 +1,9 @@
+<script src="<?= base_url('assets/js/jquery-3.3.1.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/jquery.paroller.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/jquery.custom.js') ?>"></script>
+<script src="<?= base_url('assets/js/swiper.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/swiper.custom.js') ?>"></script>
+<script src="<?= base_url('assets/js/menu.js') ?>"></script>
+
+</body>
+</html>
